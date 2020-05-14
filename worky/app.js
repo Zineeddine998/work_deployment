df610c21d2c67d672886b99996461c2bc89f5f5c
Zineeddine998 <55639522+Zineeddine998@users.noreply.github.com>
2020-05-14 10:41:43 +0100

    Add files via upload

--- worky/app.js
+++ worky/app.js
@@ -324,13 +324,7 @@ body {
               <span class="sr-only">(current)</span>
             </a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">A propos</a>
-          </li>
-         
-          <li class="nav-item">
-            <a class="nav-link" href="#">Contactez nous</a>
-          </li>
+        
         </ul>
       </div>
     </div>
@@ -356,6 +350,7 @@ body {
 
   
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
 </body>
 
